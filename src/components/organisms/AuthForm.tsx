65569413487b27a63b05f40c4ea80099c0a8f127
RuiOkazaki <okazaki200226@gmail.com>
2022-02-22 23:16:17 +0900

    update: name を userId に変更した

--- src/components/organisms/AuthForm.tsx
+++ src/components/organisms/AuthForm.tsx
@@ -1,15 +1,30 @@
 import FormButton from 'components/molecules/FormButton';
 import FormInput from 'components/molecules/FormInput';
-import { VFC } from 'react';
+import { FormEventHandler, VFC } from 'react';
 
 const AuthForm: VFC = () => {
+  const [inputState, setInputState] = useState({
+    userId: '',
+    password: '',
+  });
+  const onInput: FormEventHandler<HTMLInputElement> = ({ currentTarget }) => {
+    const { name, value } = currentTarget;
+    setInputState({ name: value });
+  };
+  // const onClick = () => {
+  //   console.log('click!');
+  // };
+  // const submit = () => {
+  //   console.log('submit!');
+  // };
   return (
     <form className='space-y-6'>
       <FormInput
-        id='personalNumber'
-        name='personalNumber'
+        id='userId'
+        name='userId'
         type='tel'
         placeholder='2000000'
+        onInput={onInput}
       >
         Student number
       </FormInput>
@@ -18,6 +33,7 @@ const AuthForm: VFC = () => {
         name='password'
         type='password'
         placeholder='Your Password'
+        onInput={onInput}
       >
         Password
       </FormInput>
