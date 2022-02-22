da4316f790b0a2a283aac02f7cdc2cc1bcec2e8a
RuiOkazaki <okazaki200226@gmail.com>
2022-02-22 17:29:12 +0900

    add: create navigation link item

---
+++ src/components/molecules/NavItem.tsx
@@ -0,0 +1,29 @@
+import { IconProp } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { VFC } from 'react';
+import { Link } from 'react-router-dom';
+
+type Props = {
+  icon: IconProp;
+  title: string;
+  path: `/${string}`;
+  selected?: boolean;
+};
+const NavItem: VFC<Props> = ({ icon, title, path, selected = false }) => {
+  return (
+    <Link
+      className={[
+        'w-full uppercase flex items-center p-4 my-2 transition-colors duration-200 justify-start',
+        selected
+          ? 'text-blue-500 bg-gradient-to-r from-white to-blue-100  border-r-4 border-blue-500'
+          : 'text-gray-500 hover:text-blue-500 hover:bg-gray-100',
+      ].join(' ')}
+      to={path}
+    >
+      <FontAwesomeIcon icon={icon} className='text-xl' />
+      <span className='mx-4 text-base font-normal'>{title}</span>
+    </Link>
+  );
+};
+
+export default NavItem;
