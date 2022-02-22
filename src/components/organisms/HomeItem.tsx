f6c082b339d245e7c6b096126d324a8df38407ed
RuiOkazaki <okazaki200226@gmail.com>
2022-02-22 17:30:31 +0900

    update: Home Page の sryle を作成した

---
+++ src/components/organisms/HomeItem.tsx
@@ -0,0 +1,34 @@
+import { IconProp } from '@fortawesome/fontawesome-svg-core';
+import { faLongArrowRight } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { ReactNode, VFC } from 'react';
+import { Link } from 'react-router-dom';
+
+type Props = {
+  title: string;
+  icon: IconProp;
+  path: `/${string}`;
+  children: ReactNode;
+};
+const HomeItem: VFC<Props> = ({ icon, title, path, children }) => {
+  return (
+    <Link
+      to={path}
+      className='flex flex-col items-center p-6 space-y-3 text-center bg-gray-100 rounded-xl duration-300 hover:shadow-lg hover:cursor-pointer'
+    >
+      <span className='block p-3 text-blue-500 bg-blue-100 rounded-full'>
+        <FontAwesomeIcon icon={icon} className='w-6 h-6 block' />
+      </span>
+      <h1 className='text-2xl font-semibold text-gray-700 capitalize'>
+        {title}
+      </h1>
+      <p className='text-gray-500'>{children}</p>
+      <div className='ml-auto pr-4 w-fit -mx-1 text-sm text-blue-500 capitalize transition-colors duration-200 transform hover:underline hover:text-blue-600'>
+        <span className='mx-1'>Learn More</span>
+        <FontAwesomeIcon icon={faLongArrowRight} />
+      </div>
+    </Link>
+  );
+};
+
+export default HomeItem;
--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,8 +1,36 @@
 import { VFC } from 'react';
 import Layout from 'components/template/Layout';
+import {
+  faTable,
+  faChartLine,
+  faBell,
+  faCalendarAlt,
+  faCog,
+} from '@fortawesome/free-solid-svg-icons';
+import HomeItem from 'components/organisms/HomeItem';
 
 const Home: VFC = () => {
-  return <Layout>hoge</Layout>;
+  return (
+    <Layout pageTitle='Home'>
+      <div className='grid grid-cols-1 gap-8 mt-8 xl:mt-12 xl:gap-8 md:grid-cols-2 xl:grid-cols-3'>
+        <HomeItem title='時間割' path='/calendar' icon={faTable}>
+          今週の時間割を確認することが出来ます。
+        </HomeItem>
+        <HomeItem title='出席率' path='/calendar' icon={faChartLine}>
+          各授業の出席率を確認することが出来ます。
+        </HomeItem>
+        <HomeItem title='学校からのお知らせ' path='/calendar' icon={faBell}>
+          学校からのお知らせを確認することが出来ます。
+        </HomeItem>
+        <HomeItem title='カレンダー' path='/calendar' icon={faCalendarAlt}>
+          今後の予定を確認することが出来ます。
+        </HomeItem>
+        <HomeItem title='設定' path='/calendar' icon={faCog}>
+          現在の設定を確認することが出来ます。
+        </HomeItem>
+      </div>
+    </Layout>
+  );
 };
 
 export default Home;
