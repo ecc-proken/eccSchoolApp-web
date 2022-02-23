a5e59e28c7a823194b5e543bd779abb569fb504b
RuiOkazaki <okazaki200226@gmail.com>
2022-02-24 00:17:40 +0900

    update: className の記述方法を変更した

--- src/components/molecules/NavItem.tsx
+++ src/components/molecules/NavItem.tsx
@@ -1,23 +1,22 @@
 import { IconProp } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { VFC } from 'react';
+import { VFC, memo } from 'react';
 import { Link } from 'react-router-dom';
 
 type Props = {
   icon: IconProp;
   title: string;
   path: `/${string}`;
-  selected?: boolean;
+  selected: boolean;
 };
 const NavItem: VFC<Props> = ({ icon, title, path, selected = false }) => {
   return (
     <Link
-      className={[
-        'w-full uppercase flex items-center p-4 my-2 transition-colors duration-200 justify-start',
+      className={`w-full uppercase flex items-center p-4 my-2 transition-colors duration-200 justify-start ${
         selected
           ? 'text-accent bg-gradient-to-r from-white to-blue-100  border-r-4 border-accent'
-          : 'text-gray-500 hover:text-accent hover:bg-gray-100',
-      ].join(' ')}
+          : 'text-gray-500 hover:text-accent hover:bg-gray-100'
+      }`}
       to={path}
     >
       <FontAwesomeIcon icon={icon} className='text-xl' />
@@ -26,4 +25,4 @@ const NavItem: VFC<Props> = ({ icon, title, path, selected = false }) => {
   );
 };
 
-export default NavItem;
+export default memo(NavItem);
