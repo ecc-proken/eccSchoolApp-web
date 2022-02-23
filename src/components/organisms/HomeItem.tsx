39abd095e3890721234b4f5a94c146f81dd47334
RuiOkazaki <okazaki200226@gmail.com>
2022-02-23 22:08:16 +0900

    update: 文字を左寄せに変更した。3文字分の高さに固定した

--- src/components/organisms/HomeItem.tsx
+++ src/components/organisms/HomeItem.tsx
@@ -22,7 +22,7 @@ const HomeItem: VFC<Props> = ({ icon, title, path, children }) => {
       <h1 className='text-2xl font-semibold text-gray-700 capitalize'>
         {title}
       </h1>
-      <p className='text-gray-500'>{children}</p>
+      <p className='text-gray-500 h-12 text-left'>{children}</p>
       <div className='ml-auto pr-4 w-fit -mx-1 text-sm text-accent capitalize transition-colors duration-200 transform hover:underline hover:text-accent'>
         <span className='mx-1'>Learn More</span>
         <FontAwesomeIcon icon={faLongArrowRight} />
