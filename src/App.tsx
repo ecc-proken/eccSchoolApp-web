28e3fa759beeeca395d500af521b496e5cb787b8
RuiOkazaki <okazaki200226@gmail.com>
2022-02-23 18:56:37 +0900

    update: 初回レンダリング時にlocalStorageからデータをatomに格納する処理を記述した

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
 import { Route, Routes } from 'react-router-dom';
-import { VFC } from 'react';
+import { useEffect, VFC } from 'react';
 import Home from './components/pages/Home';
 import Auth from './components/pages/Auth';
+import { useSetRecoilState } from 'recoil';
+import userDataState from 'globalState/userDataState';
 
 const App: VFC = () => {
+  const setUserData = useSetRecoilState(userDataState);
+  /**
+   * localStorage に格納されている userData を取得し atom に格納している。
+   * @date 2022-02-23
+   * @returns {void}
+   */
+  const getUserDataHandler = () => {
+    const userId = localStorage.getItem('userId');
+    const password = localStorage.getItem('password');
+    setUserData({ userId, password });
+  };
+  useEffect(() => getUserDataHandler(), []);
+
   return (
     <Routes>
       <Route path='/' element={<Home />} />
