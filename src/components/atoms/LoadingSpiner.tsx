8c0fab691d5290c25832a893e7a15b26b94fe262
RuiOkazaki <okazaki200226@gmail.com>
2022-02-23 20:20:01 +0900

    update: 背景色を変更した

--- src/components/atoms/LoadingSpiner.tsx
+++ src/components/atoms/LoadingSpiner.tsx
@@ -2,7 +2,7 @@ import { VFC } from 'react';
 
 const LoadingSpiner: VFC = () => {
   return (
-    <div className='flex justify-center items-center h-full w-full fixed z-10 top-0 left-0 bg-gray-700 bg-opacity-40'>
+    <div className='flex justify-center items-center h-full w-full fixed z-10 top-0 left-0 bg-gray-500 bg-opacity-30'>
       <div className='animate-spin h-10 w-10 border-4 border-accent rounded-full border-t-transparent'></div>
     </div>
   );
