9832ceb68eb92a8daed1c54116d23efdea99adff
RuiOkazaki <okazaki200226@gmail.com>
2022-02-23 20:15:48 +0900

    update: ローディング中に表示するアニメーションを作成した

---
+++ src/components/atoms/LoadingSpiner.tsx
@@ -0,0 +1,11 @@
+import { VFC } from 'react';
+
+const LoadingSpiner: VFC = () => {
+  return (
+    <div className='flex justify-center items-center h-full w-full fixed z-10 top-0 left-0 bg-gray-700 bg-opacity-40'>
+      <div className='animate-spin h-10 w-10 border-4 border-accent rounded-full border-t-transparent'></div>
+    </div>
+  );
+};
+
+export default LoadingSpiner;
