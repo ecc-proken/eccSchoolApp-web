0037ac6764a34da8214b8de83daf87f19b5ee798
RuiOkazaki <okazaki200226@gmail.com>
2022-02-24 12:01:42 +0900

    update: スマホでも綺麗に表示されるよう layout を調整した

---
+++ src/components/atoms/TableData.tsx
@@ -0,0 +1,14 @@
+import { ReactNode, VFC } from 'react';
+
+type Props = {
+  children: ReactNode;
+};
+const TableData: VFC<Props> = ({ children }) => {
+  return (
+    <td className='text-xs md:text-sm border-r text-gray-700 py-4'>
+      {children}
+    </td>
+  );
+};
+
+export default TableData;
--- src/components/organisms/Table.tsx
+++ src/components/organisms/Table.tsx
@@ -1,8 +1,9 @@
+import TableData from 'components/atoms/TableData';
 import { VFC } from 'react';
 
 const Table: VFC = () => {
   return (
-    <table className='min-w-full text-center mt-8'>
+    <table className='min-w-full text-center mt-8 h-[94%]'>
       <thead className='border bg-accent'>
         <tr>
           <th className='w-1/6 text-sm md:text-base font-bold text-white px-3 py-2 md:px-6 md:py-4'>
@@ -25,111 +26,56 @@ const Table: VFC = () => {
           </th>
         </tr>
       </thead>
-      <tbody>
+      <tbody className='h-full'>
         <tr className='bg-white border-b'>
           <td className='py-4 border-x text-xs md:text-sm text-gray-700'>
             1<br />
             <span className='text-gray-400'>09:15 ~ 10:45</span>
           </td>
-          <td className='text-xs md:text-sm border-r text-gray-700 py-4'>
-            1コマ
-          </td>
-          <td className='text-xs md:text-sm border-r text-gray-700 py-4'>
-            1コマ
-          </td>
-          <td className='text-xs md:text-sm border-r text-gray-700 py-4'>
-            1コマ
-          </td>
-          <td className='text-xs md:text-sm border-r text-gray-700 py-4'>
-            1コマ
-          </td>
-          <td className='text-xs md:text-sm border-r text-gray-700 py-4'>
-            1コマ
-          </td>
+
+          {[...Array(5)].map(() => (
+            <TableData>1コマ</TableData>
+          ))}
         </tr>
         <tr className='bg-white border-b'>
           <td className='py-4 border-x text-xs md:text-sm text-gray-700'>
             2<br />
             <span className='text-gray-400'>11:00 ~ 12:30</span>
           </td>
-          <td className='text-xs md:text-sm border-r text-gray-700 py-4'>
-            2コマ
-          </td>
-          <td className='text-xs md:text-sm border-r text-gray-700 py-4'>
-            2コマ
-          </td>
-          <td className='text-xs md:text-sm border-r text-gray-700 py-4'>
-            2コマ
-          </td>
-          <td className='text-xs md:text-sm border-r text-gray-700 py-4'>
-            2コマ
-          </td>
-          <td className='text-xs md:text-sm border-r text-gray-700 py-4'>
-            2コマ
-          </td>
+
+          {[...Array(5)].map(() => (
+            <TableData>1コマ</TableData>
+          ))}
         </tr>
         <tr className='bg-white border-b'>
           <td className='py-4 border-x text-xs md:text-sm text-gray-700'>
             3<br />
             <span className='text-gray-400'>13:30 ~ 15:00</span>
           </td>
-          <td className='text-xs md:text-sm border-r text-gray-700 py-4'>
-            3コマ
-          </td>
-          <td className='text-xs md:text-sm border-r text-gray-700 py-4'>
-            3コマ
-          </td>
-          <td className='text-xs md:text-sm border-r text-gray-700 py-4'>
-            3コマ
-          </td>
-          <td className='text-xs md:text-sm border-r text-gray-700 py-4'>
-            3コマ
-          </td>
-          <td className='text-xs md:text-sm border-r text-gray-700 py-4'>
-            3コマ
-          </td>
+
+          {[...Array(5)].map(() => (
+            <TableData>1コマ</TableData>
+          ))}
         </tr>
         <tr className='bg-white border-b'>
           <td className='py-4 border-x text-xs md:text-sm text-gray-700'>
             4<br />
             <span className='text-gray-400'>15:15 ~ 16:45</span>
           </td>
-          <td className='text-xs md:text-sm border-r text-gray-700 py-4'>
-            4コマ
-          </td>
-          <td className='text-xs md:text-sm border-r text-gray-700 py-4'>
-            4コマ
-          </td>
-          <td className='text-xs md:text-sm border-r text-gray-700 py-4'>
-            4コマ
-          </td>
-          <td className='text-xs md:text-sm border-r text-gray-700 py-4'>
-            4コマ
-          </td>
-          <td className='text-xs md:text-sm border-r text-gray-700 py-4'>
-            4コマ
-          </td>
+
+          {[...Array(5)].map(() => (
+            <TableData>1コマ</TableData>
+          ))}
         </tr>
         <tr className='bg-white border-b'>
           <td className='py-4 border-x text-xs md:text-sm text-gray-700'>
             5<br />
             <span className='text-gray-400'>17:00 ~ 18:30</span>
           </td>
-          <td className='text-xs md:text-sm border-r text-gray-700 py-4'>
-            5コマ
-          </td>
-          <td className='text-xs md:text-sm border-r text-gray-700 py-4'>
-            5コマ
-          </td>
-          <td className='text-xs md:text-sm border-r text-gray-700 py-4'>
-            5コマ
-          </td>
-          <td className='text-xs md:text-sm border-r text-gray-700 py-4'>
-            5コマ
-          </td>
-          <td className='text-xs md:text-sm border-r text-gray-700 py-4'>
-            5コマ
-          </td>
+
+          {[...Array(5)].map(() => (
+            <TableData>1コマ</TableData>
+          ))}
         </tr>
       </tbody>
     </table>
