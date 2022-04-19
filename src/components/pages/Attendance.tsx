b703509ec14392babc87e714c9e91d8c871c8c7a
RuiOkazaki <okazaki200226@gmail.com>
2022-04-19 14:03:23 +0900

    feat: 出席率を確認できるページを作成した

--- src/components/pages/Attendance.tsx
+++ src/components/pages/Attendance.tsx
@@ -1,11 +1,13 @@
 import Layout from 'components/template/Layout';
 import Title from 'components/template/Title';
 import { VFC } from 'react';
+import AttendanceCharts from 'components/organisms/AttendanceCharts';
 
 const Attendance: VFC = () => {
   return (
     <Layout pageTitle='出席率'>
       <Title pageTitle='出席率' />
+      <AttendanceCharts />
     </Layout>
   );
 };
