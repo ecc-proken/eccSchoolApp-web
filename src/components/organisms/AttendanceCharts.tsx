60a83ddaa5715b8565114b4170a4271b2b56fabb
RuiOkazaki <okazaki200226@gmail.com>
2022-04-19 14:27:18 +0900

    feat: 円グラフを作成した

--- src/components/organisms/AttendanceCharts.tsx
+++ src/components/organisms/AttendanceCharts.tsx
@@ -3,17 +3,46 @@ import useGetAttendance from 'hooks/useGetAttendance';
 import { VFC } from 'react';
 import { useQueryClient } from 'react-query';
 import Attendance from 'types/attendance';
+import {
+  Radar,
+  RadarChart,
+  PolarGrid,
+  PolarAngleAxis,
+  PolarRadiusAxis,
+} from 'recharts';
 
 const AttendanceCharts: VFC = () => {
   const queryClient = useQueryClient();
   const cacheData = queryClient.getQueryData<Attendance[]>('attendance');
   const { data, isLoading } = useGetAttendance();
   const attendanceData = cacheData || data;
-  // eslint-disable-next-line no-console
-  console.log(attendanceData);
 
-  // eslint-disable-next-line react/jsx-no-useless-fragment
-  return <>{isLoading && <LoadingSpiner />}</>;
+  return (
+    <>
+      {isLoading && <LoadingSpiner />}
+      <RadarChart
+        cx={300}
+        cy={250}
+        outerRadius={150}
+        width={500}
+        height={500}
+        data={attendanceData?.map((d) => ({
+          title: d.title,
+          rate: Number(d.rate.slice(0, -1)),
+        }))}
+      >
+        <PolarGrid />
+        <PolarAngleAxis dataKey='title' />
+        <PolarRadiusAxis />
+        <Radar
+          dataKey='rate'
+          stroke='#8884d8'
+          fill='#8884d8'
+          fillOpacity={0.6}
+        />
+      </RadarChart>
+    </>
+  );
 };
 
 export default AttendanceCharts;
