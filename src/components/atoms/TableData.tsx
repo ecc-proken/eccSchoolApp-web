2e37fcfebbe838125e3f563375212be93ae8cbeb
OkazakiRui <okazaki200226@gmail.com>
2022-06-26 00:36:11 +0900

    feat: 出席率を表示可能にした

--- src/components/atoms/TableData.tsx
+++ src/components/atoms/TableData.tsx
@@ -4,8 +4,9 @@ import { subjectData } from 'types/timetable';
 type Props = {
   tableData?: subjectData;
   selected?: boolean;
+  rate?: string;
 };
-const TableData: VFC<Props> = ({ tableData, selected = false }) => {
+const TableData: VFC<Props> = ({ tableData, selected = false, rate }) => {
   return (
     <td
       className={`text-[10px] md:text-[0.9vw] border-r text-gray-700 h-[14vh] md:h-[18vh] px-1 ${
@@ -13,13 +14,21 @@ const TableData: VFC<Props> = ({ tableData, selected = false }) => {
       }`}
     >
       <p>{tableData?.subjectTitle}</p>
-      <span className='text-[10px] md:text-[0.8vw] text-gray-400'>
-        {tableData?.teacher.replace('　', ' ')}
-      </span>
-      <br />
-      <span className='text-[10px] md:text-[0.8vw] text-accent font-bold'>
-        {tableData?.classroom}
-      </span>
+      {rate !== undefined ? (
+        <span className='text-[10px] md:text-[0.8vw] text-gray-400'>
+          出席率: <span className='text-accent font-bold'>{rate}</span>
+        </span>
+      ) : (
+        <>
+          <span className='text-[10px] md:text-[0.8vw] text-gray-400'>
+            {tableData?.teacher.replace('　', ' ')}
+          </span>
+          <br />
+          <span className='text-[10px] md:text-[0.8vw] text-accent font-bold'>
+            {tableData?.classroom}
+          </span>
+        </>
+      )}
     </td>
   );
 };
