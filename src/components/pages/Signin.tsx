44dba26684c86c2ef086e50bc77043feea149c45
OkazakiRui <okazaki200226@gmail.com>
2022-06-26 23:10:05 +0900

    feat: text等を修正した

--- src/components/pages/Signin.tsx
+++ src/components/pages/Signin.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useState, VFC } from 'react';
 // ToDo: imgを変更する
-import heroImg from 'assets/auth_heroImage.svg';
 import AuthForm from 'components/organisms/AuthForm';
 import Title from 'components/template/Title';
 import { useRecoilState, useResetRecoilState } from 'recoil';
@@ -9,6 +8,7 @@ import axios from 'axios';
 import LoadingSpiner from 'components/atoms/LoadingSpiner';
 import { useNavigate } from 'react-router-dom';
 import AnimationDiv from 'components/template/AnimationDiv';
+import authHeroImage from 'assets/auth_heroImage.jpeg';
 
 const Signin: VFC = () => {
   const [userData, setUserData] = useRecoilState(userDataState);
@@ -46,21 +46,20 @@ const Signin: VFC = () => {
           <div
             className='flex items-center h-full px-20 bg-gray-100 bg-no-repeat'
             style={{
-              backgroundImage: `url(${heroImg})`,
-              backgroundSize: '50%',
-              backgroundPosition: 'center right 10%',
+              backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.3)), url(${authHeroImage})`,
+              backgroundSize: 'cover',
+              backgroundPosition: 'center',
             }}
           >
             <div>
-              <h2 className='text-4xl font-bold text-gray-700'>
-                ECCcomp School App
+              <h2 className='text-4xl font-bold text-gray-50'>
+                <span className='inline-block'>ECCコンピュータ専門学校</span>{' '}
+                <span className='inline-block'>非公式アプリ</span>
               </h2>
-
-              {/* ToDo: 画像にあったサイズに変更する */}
-              <p className='max-w-md mt-3 text-gray-500'>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. In
-                autem ipsa, nulla laboriosam dolores, repellendus perferendis
-                libero suscipit nam temporibus molestiae
+              <p className='max-w-2xl mt-3 text-gray-50'>
+                ECCコンピュータ専門学校のアプリをより使いやすく、学生の欲しい機能を詰め込んだWEBアプリケーションです。
+                <br />
+                時間割や出席率の確認が簡単にできます。
               </p>
             </div>
           </div>
@@ -70,8 +69,9 @@ const Signin: VFC = () => {
           <div className='flex-1'>
             <div className='text-center'>
               <h2 className='text-4xl space-y-2 font-bold text-center text-gray-700'>
-                <span className='inline-block'>ECCcomp</span>{' '}
-                <span className='inline-block'>School App</span>
+                <span className='inline-block'>ECCコンピュータ</span>
+                <span className='inline-block'>専門学校</span>{' '}
+                <span className='inline-block'>非公式アプリ</span>
               </h2>
 
               <p className='mt-3 text-gray-500'>
