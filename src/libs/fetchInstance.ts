6c4e019e04775cc15be915af7daaf623e7bbf021
RuiOkazaki <okazaki200226@gmail.com>
2022-11-20 21:52:56 +0900

    feat: pathにuuidを入れた

--- src/libs/fetchInstance.ts
+++ src/libs/fetchInstance.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import User from 'types/user';
 
 export const fetchInstance = () => {
   if (process.env.REACT_APP_API_URL === undefined)
@@ -12,12 +13,12 @@ export const fetchInstance = () => {
   });
 };
 
-export const fetchWithToken = ({ token }: { token: string }) => {
+export const fetchWithToken = ({ token, uuid }: User) => {
   if (process.env.REACT_APP_API_URL === undefined)
     throw new Error('API_URL is not defined');
 
   return axios.create({
-    baseURL: process.env.REACT_APP_API_URL,
+    baseURL: `${process.env.REACT_APP_API_URL}/${uuid}`,
     headers: {
       'Content-Type': 'application/json',
       Authorization: `Bearer ${token}`,
