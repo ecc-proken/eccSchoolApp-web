3b5f38835de175cb9735da01d25f35b44aba1e78
RuiOkazaki <okazaki200226@gmail.com>
2023-02-11 00:47:35 +0900

    feat: uiを作成した＼

--- src/components/organisms/AttendanceTable.tsx
+++ src/components/organisms/AttendanceTable.tsx
@@ -16,44 +16,67 @@ const AttendanceTable: VFC = () => {
       {attendanceData === undefined ? (
         !isLoading && '出席率の取得に失敗しました'
       ) : (
-        <div className='relative overflow-x-auto shadow-md sm:rounded-lg'>
+        <div className='relative overflow-x-auto shadow-sm sm:rounded-lg'>
           <table className='w-full text-sm text-left'>
-            <thead className='text-xs text-white uppercase bg-accent'>
+            <thead className='text-sm text-white uppercase bg-accent'>
               <tr>
-                <th scope='col' className='px-6 py-3'>
+                <th
+                  scope='col'
+                  className='px-3 py-3 xl:px-6 xl:py-3 whitespace-nowrap font-normal'
+                >
                   授業名
                 </th>
-                <th scope='col' className='text-right px-6 py-3'>
+                <th
+                  scope='col'
+                  className='text-right px-3 py-3 xl:px-6 xl:py-3 whitespace-nowrap font-normal'
+                >
                   出席率
                 </th>
-                <th scope='col' className='text-right px-6 py-3'>
+                <th
+                  scope='col'
+                  className='text-right px-3 py-3 xl:px-6 xl:py-3 whitespace-nowrap font-normal'
+                >
                   欠席回数
                 </th>
-                <th scope='col' className='text-right px-6 py-3'>
+                <th
+                  scope='col'
+                  className='text-right px-3 py-3 xl:px-6 xl:py-3 whitespace-nowrap font-normal'
+                >
                   遅刻回数
                 </th>
-                <th scope='col' className='text-right px-6 py-3'>
+                <th
+                  scope='col'
+                  className='text-right px-3 py-3 xl:px-6 xl:py-3 whitespace-nowrap font-normal'
+                >
                   出席回数
                 </th>
               </tr>
             </thead>
             <tbody>
-              {attendanceData.map((attendance) => (
-                <tr className='bg-white border-b'>
+              {attendanceData.map((attendance, key) => (
+                <tr
+                  className={`border-b ${
+                    key % 2 === 0 ? 'bg-white' : 'bg-sky-100'
+                  }`}
+                >
                   <th
                     scope='row'
-                    className='px-6 py-4 font-medium text-gray-900 whitespace-nowrap'
+                    className='px-3 py-3 xl:px-6 xl:py-4 font-medium whitespace-nowrap'
                   >
                     {attendance.title}
                   </th>
-                  <td className='px-6 py-4 text-right'>{attendance.rate}</td>
-                  <td className='px-6 py-4 text-right'>
+                  <td className='px-3 py-3 xl:px-6 xl:py-4 text-right whitespace-nowrap'>
+                    {attendance.rate}
+                  </td>
+                  <td className='px-3 py-3 xl:px-6 xl:py-4 text-right whitespace-nowrap'>
                     {attendance.absence}回
                   </td>
-                  <td className='px-6 py-4 text-right'>
+                  <td className='px-3 py-3 xl:px-6 xl:py-4 text-right whitespace-nowrap'>
                     {attendance.lateness}回
                   </td>
-                  <td className='px-6 py-4 text-right'>{attendance.count}回</td>
+                  <td className='px-3 py-3 xl:px-6 xl:py-4 text-right whitespace-nowrap'>
+                    {attendance.count}回
+                  </td>
                 </tr>
               ))}
             </tbody>
